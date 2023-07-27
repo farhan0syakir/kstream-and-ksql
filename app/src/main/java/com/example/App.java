6a8583569fe76fa3d96a148557272bd26437a397
Farhan.Syakir <farhan.syakir@luminorgroup.com>
2023-07-27 14:38:40 +0300

    add stream

--- app/src/main/java/com/example/App.java
+++ app/src/main/java/com/example/App.java
@@ -3,12 +3,41 @@
  */
 package com.example;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Properties;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<Void, String> stream = builder.stream("users");
+        stream.foreach(
+        (key, value) -> {
+            System.out.println("(DSL) Hello, " + value);
+        });
+
+        // omitted for brevity
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "dev1");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Void().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+        streams.start();
+
+        // close Kafka Streams when the JVM shuts down (e.g., SIGTERM)
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 }
